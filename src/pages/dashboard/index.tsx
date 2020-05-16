be82b387ffb52c5532c2bc12f9fb2501f4cdada5
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-05-16 20:33:53 -0300

    implement dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { FiChevronRight } from 'react-icons/fi';
 import { Title,Form,Repositories } from './styles';
 //react function component
 
@@ -14,13 +14,36 @@ const Dashboard: React.FC = () => {
             <Repositories>
                 <a href="teste">
                     <img
-                        src="#"
+                        src="https://avatars0.githubusercontent.com/u/37042672?s=460&u=dd7ea84516915c0b161d24fa7841a8f6cd9ca79b&v=4"
                         alt="Juliherms Vasconcelos">
                     </img>
                     <div>
                         <strong>Microservicos</strong>
                         <p>teste stes teste teste</p>
                     </div>
+                    <FiChevronRight size={20}></FiChevronRight>
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars0.githubusercontent.com/u/37042672?s=460&u=dd7ea84516915c0b161d24fa7841a8f6cd9ca79b&v=4"
+                        alt="Juliherms Vasconcelos">
+                    </img>
+                    <div>
+                        <strong>Microservicos</strong>
+                        <p>teste stes teste teste</p>
+                    </div>
+                    <FiChevronRight size={20}></FiChevronRight>
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars0.githubusercontent.com/u/37042672?s=460&u=dd7ea84516915c0b161d24fa7841a8f6cd9ca79b&v=4"
+                        alt="Juliherms Vasconcelos">
+                    </img>
+                    <div>
+                        <strong>Microservicos</strong>
+                        <p>teste stes teste teste</p>
+                    </div>
+                    <FiChevronRight size={20}></FiChevronRight>
                 </a>
             </Repositories>
         </>
--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -13,36 +13,44 @@ export const Title = styled.h1`
 
 
 export const Form = styled.form`
-    margin-top: 40px;
-    max-width:700px;
-    display:flex;
-   
-    input : {
-        flex: 1;
-        height:170px;
-        padding: 0 24px;
-        border:0;
-        border-radius: 5px 0 0 5px;
-        color:#3a3a3a;
 
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+    
+    &::placeholder {
+      color: #a8a8b3;
     }
 
-    button {
-        width:210px;
-        background: #04d361;
-        border-radius: 0px 5px 5px 0px;
-        border:0;
-        color:#FFF;
-        font-weight:bold;
-        transition: background-color 0.2s;
+  }
 
-        &:hover {
-            background: ${shade(0.2,'#04d361')};
-        }
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
     }
+    
+  }
 `;
 
-export const Repositories = styled.form`
+export const Repositories = styled.div`
     margin-top:80px;
     max-width: 700px;
 
@@ -55,6 +63,15 @@ export const Repositories = styled.form`
         text-decoration:none;
         display:flex;
         align-items:center;
+        transition:transform 0.2s;
+
+        & + a {
+            margin-top: 16px;
+        }
+
+        &:hover {
+            transform: translateX(10px);
+        }
 
         img {
             width:64px;
@@ -76,5 +93,10 @@ export const Repositories = styled.form`
                 margin-top:4px;
             }
         }
+
+        svg {
+            margin-left:auto;
+            color:#cbcbd6;
+        }   
     }
 `;
