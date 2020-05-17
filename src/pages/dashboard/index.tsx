79a79f716bf36685bf79defd194572c5902787f1
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-05-17 11:11:46 -0300

    implement form hasError with css

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -49,7 +49,7 @@ const Dashboard: React.FC = () => {
             setNewRepo('');
             setInputError('');
         } catch (err){
-            setInputError('Erro with repository search');
+            setInputError('Error with repository search');
         }
     }
 
@@ -64,7 +64,7 @@ const Dashboard: React.FC = () => {
                 <button type="submit">Search</button>
             </Form>
 
-        {inputError && <Error>inputError</Error>}
+        {inputError && <Error>{inputError}</Error>}
 
             <Repositories>
                 {repositories.map(repository => (
